bd416811c648f1cf85658a36c394fc58fbb1c718
arobey <sumiyaakter.arobey@gmail.com>
2025-05-01 19:33:43 +0600

    variables

---
+++ dist/variable.js
@@ -0,0 +1,8 @@
+"use strict";
+let greetings = "Hello hri";
+greetings.toLowerCase();
+console.log(greetings);
+let userId = 334466.5;
+userId.toFixed();
+let isLoggedIn = false;
+//# sourceMappingURL=variable.js.map
---
+++ src/variable.ts
@@ -0,0 +1,17 @@
+let greetings: string = "Hello hri";
+greetings.toLowerCase();
+console.log(greetings);
+
+//number
+let userId = 334466.5;
+userId.toFixed(); 
+
+//boolean
+let isLoggedIn: boolean = false
+
+//any
+let hero: string;
+function getHero(){
+  return "thor";
+}
+hero = getHero();
