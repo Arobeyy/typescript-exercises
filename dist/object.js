cf4f43e3b772abdc6d7ec13bcf5cdfb3c526ef4c
arobey <sumiyaakter.arobey@gmail.com>
2025-05-02 20:12:01 +0600

    ReadOnly and Optional

--- dist/object.js
+++ dist/object.js
@@ -1,10 +1,9 @@
 "use strict";
-const User = {
+let myUser = {
+    _id: "1234",
     name: "su",
-    email: "s@s.dev",
-    isActive: true,
+    email: "s@s.com",
+    isActive: false,
 };
-function createCourse() {
-    return { name: "typeScript", price: 399 };
-}
+myUser.email = "s@gmail.com";
 //# sourceMappingURL=object.js.map
--- src/object.ts
+++ src/object.ts
@@ -7,20 +7,51 @@
 //function createUser({ name: string, isPaid: boolean }) {}
 //createUser({ name: "hri", isPaid: true });
 
-
 // returns an object
 //function createCourse(): {name: string, price: number} {
 //  return {name: "typeScript", price: 399}
 //}
 
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean
+// }
+
+// function createUser(user: User): User {
+//   return user;
+// }
+
+// createUser({name: '', email: "", isActive: true});
+
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
-  isActive: boolean
-}
+  isActive: boolean;
+  craditCardDetails?: number;
+};
 
-function createUser(user: User): User {
-  return user;
-}
+let myUser: User = {
+  _id: "1234",
+  name: "su",
+  email: "s@s.com",
+  isActive: false,
+};
 
-createUser({name: '', email: "", isActive: true});
+myUser.email = "s@gmail.com";
+//myUser._id = "456";
+
+type cardNumber = {
+  cardNum: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
+ 
