b23400fb9dfd68b76e219b05d60aa664a6ad2ca8
arobey <sumiyaakter.arobey@gmail.com>
2025-05-02 19:39:33 +0600

    Type Aliases

--- src/object.ts
+++ src/object.ts
@@ -1,14 +1,26 @@
-const User = {
-  name: "su",
-  email: "s@s.dev",
-  isActive: true,
-};
+//const User = {
+//  name: "su",
+//  email: "s@s.dev",
+//  isActive: true,
+//};
 
 //function createUser({ name: string, isPaid: boolean }) {}
 //createUser({ name: "hri", isPaid: true });
 
 
 // returns an object
-function createCourse(): {name: string, price: number} {
-  return {name: "typeScript", price: 399}
+//function createCourse(): {name: string, price: number} {
+//  return {name: "typeScript", price: 399}
+//}
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean
 }
+
+function createUser(user: User): User {
+  return user;
+}
+
+createUser({name: '', email: "", isActive: true});
