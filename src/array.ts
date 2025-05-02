db7eefb88d4b2fa38701f9be3e6bbce152342dd3
arobey <sumiyaakter.arobey@gmail.com>
2025-05-02 20:23:48 +0600

    Array basic

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = [];
+//const heroHP: number[] = [];
+const heroHP: Array<number> = [];
+
+superHeros.push("Ruber Ducky");
+heroHP.push(10);
+
+type myUser = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: myUser[] = [];
+
+allUsers.push({ name: "su", isActive: true });
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [255, 256, 255],
+];
