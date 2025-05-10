218086475be05040f0ad58b9d83079d5fe41446a
arobey <sumiyaakter.arobey@gmail.com>
2025-05-10 21:36:01 +0600

    tuples

---
+++ src/tuples.ts
@@ -0,0 +1,13 @@
+//const user: (string | number) [] = [1, 'su'];
+let user:[string, number, boolean]
+
+user = ['su', 132, true]
+
+let rgb: [number, number, number] = [255, 123, 152]
+
+type User2 = [number, string];
+
+const newUser: User2 = [121, 'example@google.com']
+
+newUser[1] = 'g@g.com'
+// newUser.push(true);
